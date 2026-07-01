c25a780b2469f53ad2822fa48062874c3da8e35d
shaktimishra718-cloud <shaktimishra718@gmail.com>
2026-07-01 16:52:35 +0530

    API INTEGRATION DUNCTION
    Updated the prediction function to include competition name and neutral venue flag.

--- api_integrate.py
+++ api_integrate.py
@@ -1,28 +1,28 @@
-from google.colab import userdata
-import requests
-import pandas as pd
-import time
-from sklearn.ensemble import RandomForestClassifier
-
-def prediction(api_key, league_id, season , upcoming_only):
-  url = "https://v3.football.api-sports.io/fixtures"
-  headers = {"x-apisports-key":api_key}
-  if upcoming_only:
-    querystring = {"league" : league_id, "season" : season, "next" : "10"}
-  else:
-    querystring = {"league" : league_id , "season": season , "last": "50"}#Fetch last 50 match for training
-
-  response = requests.get(url, headers = headers , params = querystring)
-  if response.status_code != 200 or response.json().get("errors"):
-        print(f"Error fetching data. Check your API key or limits.")
-        return None
-  for item in response.json().get("response" , []):
-    matches.append({
-        "fixture_id": item["fixture"]["id"],
-        "home_team": item["teams"]["home"]["name"],
-        "away_team": item["teams"]["away"]["name"],
-        "home_goals": item["goals"]["home"],
-        "away_goals": item["goals"]["away"]
-      })
+def prediction(api_key, league_id, comp_name, is_neutral, season, upcoming_only):
+    # Define the API endpoint and headers
+    url = "https://v3.football.api-sports.io/fixtures"
+    headers = {"x-apisports-key": api_key}
+    
+    if upcoming_only:
+        querystring = {"league": league_id, "season": season, "next": "10"}
+    else:
+        querystring = {"league": league_id, "season": season, "last": "40"} # Pull last 40 games per league
+        
+    response = requests.get(url, headers=headers, params=querystring)
+    
+    if response.status_code != 200 or response.json().get("errors"):
+        print(f"Skipping {comp_name}: API limit reached or invalid response.")
+        return pd.DataFrame()
+        
+    matches = []
+    for item in response.json().get("response", []):
+        matches.append({
+            "fixture_id": item["fixture"]["id"],
+            "competition": comp_name,
+            "home_team": item["teams"]["home"]["name"],
+            "away_team": item["teams"]["away"]["name"],
+            "is_neutral_venue": int(is_neutral), # 1 for True, 0 for False
+            "home_goals": item["goals"]["home"],
+            "away_goals": item["goals"]["away"]
+        })
     return pd.DataFrame(matches)
-userdata.get('APISPORTS_KEY')
